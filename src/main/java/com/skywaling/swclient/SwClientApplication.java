225860cd8cc708be70b6ac8cb3c738efa4cac5a0
otailai <otailai@163.com>
2020-06-10 10:28:35 +0800

    init

---
+++ src/main/java/com/skywaling/swclient/SwClientApplication.java
@@ -0,0 +1,13 @@
+package com.skywaling.swclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwClientApplication.class, args);
+    }
+
+}
